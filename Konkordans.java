f34ad44b283c062166b713289d2c78f3165cedf8
Therese Askling <taskling@kth.se>
2013-09-17 09:30:11 +0200

    Kommnenterad kod

--- Konkordans.java
+++ Konkordans.java
@@ -1,3 +1,11 @@
+/**
+* A program creating a concordance-database from a text to be used with the 
+* command java Konkordans word-you-wish-to-look-up
+*
+* @author Therese Askling, taskling@kth.se
+* @author Matilda Valleryd, valleryd@kth.se
+*/
+
 import java.util.*;
 import java.io.*;
 public class Konkordans{
@@ -5,53 +13,53 @@ public class Konkordans{
 	public static void main(String[] args) {
 		// Initiating konkordans object
 		Konkordans app = new Konkordans();
-		// names of files used
-		String file = "ut.txt";
-		String aIndex = "aIndex.txt";
-		String txtFile = "korpus.txt";
+		// The files used
+		String tokenizerOutput = "/var/tmp/ut.txt";
+		String aIndexFile = "aIndex.txt";
+		String korpus = "/info/adk13/labb1/korpus";
 
-		// create hashtable for index
-		Hashtable<String, Long> index = new Hashtable<String, Long>();
-		
-		// if index has been written to file, read it from file
-		if(new File(aIndex).isFile()){	
-			index = app.readIndexFromFile(aIndex);
-		// otherwise, create index and write it to file
+		// create a hashtable for the a-index
+		Hashtable<String, Long> aIndex = new Hashtable<String, Long>();
+		// if the index has previously been written to a file, create a hashtable from it
+		if(new File(aIndexFile).isFile()){	
+			aIndex = app.readIndexFromFile(aIndexFile);
+		// if not, create the index and save it to the file aIndexFile
 		}else{
-			index = app.createIndex(file, index);
-			app.indexToFile(index, aIndex);
+			aIndex = app.createAIndex(tokenizerOutput, aIndex);
+			app.indexToFile(aIndex, aIndexFile);
 		}
-		// list for range of indexes in "ut.txt"
+		// initilize a list to contain where to start and stop the to be search
 		long[] in = {0,0};
-		// check to see that the user has provided arguments
+		// If the user has provided a word to be searched, get it
 		if(args.length>0){
 			args[0] = args[0].toLowerCase();
-			// get the range of the indexes
-			in = app.getRange(file, index, args[0]);	
+			// get the start- and stop-indexes to be searched
+			in = app.getRange(tokenizerOutput, aIndex, args[0]);	
 			// create Linkedlist for the word positions
-			LinkedList<Long> x = app.findWordPositions(args[0],file, in);
+			LinkedList<Long> x = app.findWordPositions(args[0],tokenizerOutput, in);
 			// Print number of words in text
 			System.out.println("Det finns "+ x.size() + " förekomster av ordet.");
 			// if the number of words in the text is more than 25, ask the user 
 			// if the word in the sentence should be printed.
 			if(x.size()>25){
 				if(app.askUser("Vill du skriva ut alla förekomster? \nJ(Ja) or N(Nej)")){
-					app.writeSentences(x, txtFile, args[0].length());
+					app.writeSentences(x, korpus, args[0].length());
 				}
 			}
 			// if the number of words in the text is less than 25, but more than 0
 			// print the word with sentence
 			else if (x.size()> 0){
-				app.writeSentences(x, txtFile, args[0].length());
+				app.writeSentences(x, korpus, args[0].length());
 			}
 		};
 		
 	};
-	/* Find all the positions of the word that you are looking for from a indexfile
+	/** Find all the positions of the word that you are looking for from a indexfile
 	* Parameters: word you are looking for, indexfile-name, array with two long 
 	* numbers as the range in which look for the word
-	* Returns: a LinkedList of all the positions where the word exists stored as long numbers*/
-	public LinkedList<Long> findWordPositions(String word, String file, long[] in){
+	* Returns: a LinkedList of all the positions where the word exists stored as long numbers
+	*/
+	public LinkedList<Long> findWordPositions(String word, String tokenizerOutput, long[] in){
 		// Create local varibles
 		int i = (int)in[0];
 		int j = (int)in[1];
@@ -61,8 +69,8 @@ public class Konkordans{
 		char c;
 		String parts[];
 		try {
-			// Open file as randomaccessfile with read access
-			RandomAccessFile reader = new RandomAccessFile(file, "r");
+			// Open tokenizerOutput as randomaccessfile with read access
+			RandomAccessFile reader = new RandomAccessFile(tokenizerOutput, "r");
 			// While the range to look in is longer than 1000
 			while ((j-i)>1000){
 				// give m a value in the middle of j and i
@@ -123,22 +131,35 @@ public class Konkordans{
 		return x;
 	}
 
-	public long[] getRange(String file, Hashtable<String, Long> index, String word){
+	/**
+	* Uses the A-index to find the first and last index in the tokenizerOutput file 
+	* that the program will search to find the given word.
+	* Uses the method "incremented" to get the next available 3-letter combination and
+	* uses that value to get the index where to stop the search.
+	*
+	* @param aIndex
+	* the hashtable containing the A-index
+	* @param word 
+	* the word we are getting the range for
+	* @return the range as a list of 2 longs containing start- and stop-index
+	*/
+	public long[] getRange(String file, Hashtable<String, Long> aIndex, String word){
 		long[] in = {0,0};
+		
+		//Get the first 3 letters of the word:
 		if(word.length()> 3){
 			word = word.substring(0,3);
 		}
-		if(index.containsKey(word)){
-			in[0] = index.get(word);
+		// Get the index corresponding to the correct 3-letter combination
+		if(aIndex.containsKey(word)){
+			in[0] = aIndex.get(word);
+			//Use incremented to find the next available 3-letter combination
 			word = incremented(word);
-			while(!(index.containsKey(word))){
+			while(!(aIndex.containsKey(word))){
 				word = incremented(word);
 			}
-			word = incremented(word);
-			while(!(index.containsKey(word))){
-				word = incremented(word);
-			}
-			in[1] = index.get(word);
+
+			in[1] = aIndex.get(word);
 			return in;
 		}
 		return in;
@@ -146,7 +167,18 @@ public class Konkordans{
 	};
 
 
-	public Hashtable<String, Long> createIndex(String file, Hashtable<String, Long> index){
+	/**
+	* Method used to create an A-index which contains 3-letter combinations and an
+	* index to the first word begining with each combination. 
+	*
+	* @param file
+	* A file containing on each row, a keyword (and an index but we will not use this
+	* index here). 
+	* @param aIndex
+	* A hashtable containing each 3-letter (or fewer) combination exsisting in the file 
+	* and an index to where in the input-file the first word begining with it is. 
+	*/
+	public Hashtable<String, Long> createAIndex(String file, Hashtable<String, Long> aIndex){
 		try {
 			RandomAccessFile reader = new RandomAccessFile(file, "r");
 			String line = null;
@@ -160,19 +192,27 @@ public class Konkordans{
         			}else{
         				sub = parts[0].substring(0, 3);
         			}
-        			if (index.containsKey(sub)){
+        			if (aIndex.containsKey(sub)){
         				continue;
         			}
-        			index.put(sub, pointer);
+        			aIndex.put(sub, pointer);
         			pointer = reader.getFilePointer();
     			};
     		reader.close();
 		} catch (IOException x) {
     	System.err.println(x);
 		}
-		return index;
+		return aIndex;
 	}
 
+	/**
+	* Helpmethod for "getRange" used to find the next available 3-letter 
+	* combination.
+	*
+	* @param original
+	* original 3-letter combination
+	* @return next available 3-letter combination
+	*/
 	public String incremented(String original) {
     	StringBuilder buf = new StringBuilder(original);
    		int index = buf.length() -1;
@@ -191,6 +231,14 @@ public class Konkordans{
     	return buf.toString();
 	}
 
+	/**
+	* Writes a hastable to a file
+	*
+	* @param index
+	* hashtable we wish to write to a file
+	* @param file
+	* file we wish to write to
+	*/
 	public void indexToFile(Hashtable<String, Long>index, String file){
 		Enumeration<String> keys = index.keys();
 		try{
@@ -205,6 +253,14 @@ public class Konkordans{
 		}
 	}
 
+	/**
+	* Gets an index from an existing file by reading each line, splitting it into keyword and
+	* index and saves each set in a hashtable which is returned.
+	*
+	* @param file
+	* The file that is storing the keywords and indexes
+	* @return the input file as a hashtable with keywords and indexes
+	*/
 	public Hashtable<String, Long> readIndexFromFile(String file){
 		Hashtable<String, Long> index = new Hashtable<String, Long>();
 		String line = null;
@@ -226,6 +282,16 @@ public class Konkordans{
 		return index;
 	}
 
+	/**
+	* For each index in the linked list this method prints out the 30 chars
+	* before the word begining at that index, the word and then the 30 chars after
+	*
+	* @param list
+	* the linked list containing the position of each instance of the current word
+	* @param wordLenght
+	* lenght to be added to the trail of characters behind the word so that it really
+	* prints the word + 30 chars
+	*/
 	public void writeSentences(LinkedList<Long> list, String file, int wordLength){
 		byte[] b = new byte[(60 + wordLength)];
 		int size = list.size();
@@ -247,6 +313,13 @@ public class Konkordans{
 
 	}
 
+	/**
+	* Prints a question to the user and returns the answer given
+	*
+	* @param question
+	* A string containing the question to be asked
+	* @return the answer given by the user 
+	*/
 	public boolean askUser(String question){
 		Scanner sc = new Scanner(System.in);
 		System.out.println(question);
